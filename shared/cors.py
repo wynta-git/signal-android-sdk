f3383486b4f415ed8abf3b8bb94d23aca10f33cf
Dhanasekar <dhanasekaran.d@gridlogic.in>
2026-06-05 21:32:17 +0530

    cors url added

--- shared/cors.py
+++ shared/cors.py
@@ -4,4 +4,7 @@ CORS_ORIGINS: list[str] = [
     "http://localhost:3001",
     "http://127.0.0.1:3001",
     "http://127.0.0.1:8081",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://staging.wynta.com",
 ]
